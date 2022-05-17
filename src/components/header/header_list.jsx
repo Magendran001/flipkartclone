eaa668918babb6b776cdc9de50cd2cfbc8710983
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-05-17 20:10:45 +0530

    tooltip static done

--- src/components/header/header_list.jsx
+++ src/components/header/header_list.jsx
@@ -4,13 +4,18 @@ import {Link} from "react-router-dom"
 
 import { MdOutlineArrowDropUp,MdOutlineArrowDropDown } from "react-icons/md";
 import { Fashion,Electronics,Home,Appliances,Toys_others } from "./header_sub_components_array";
+import { useDispatch } from "react-redux";
+import { subcategoriespass } from "../../redux/tooltipreducer.jsx/action";
+
 import { useState } from "react";
+
 
 
 
 
 function Headerlist({data})
 {
+  let dispatch = useDispatch();
 
   let [props,setprops] = useState(Fashion);
   
@@ -19,25 +24,31 @@ function Headerlist({data})
     
      if(value=="Fashion")
      {
-      setprops(Fashion)
+      setprops(Fashion);
+      dispatch(subcategoriespass(Fashion[0].subcategories))
      }
      else if(value =="Electronics")
      {
        
       setprops(Electronics)
+      dispatch(subcategoriespass(Electronics[0].subcategories))
      }
      else if(value =="Home")
      {
 
       setprops(Home)
+      dispatch(subcategoriespass(Home[0].subcategories))
      }
      else if(value =="Appliances")
      {
       setprops(Appliances)
+      dispatch(subcategoriespass(Appliances[0].subcategories))
      }
      else if(value =="Beauty,Toys & More")
      {
       setprops(Toys_others)
+      dispatch(subcategoriespass(Toys_others[0].subcategories))
+      
      }
     
    
--- src/components/header/headertooltip/headertooltip.jsx
+++ src/components/header/headertooltip/headertooltip.jsx
@@ -1,10 +1,15 @@
 import "./headertooltip.css";
+import "../../commoncss/common.css"
 import { Fashion } from "../header_sub_components_array";
 import { useEffect, useState } from "react";
 import { useDispatch,useSelector } from "react-redux";
 import { subcategoriespass } from "../../../redux/tooltipreducer.jsx/action";
 import store from "../../../redux/store";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AiOutlineRight } from "react-icons/ai";
+
+
 
 function Headertooltip({data})
 {
@@ -13,11 +18,11 @@ function Headertooltip({data})
     let Fashion = data;
     
     //   selector value is nothing but array of obj used for tooltip second show realted more
-     let Selector = useSelector((store)=>{return store.tooltipsubcategories});
+     let Selector = useSelector((store)=>{return store.tooltipsubcategories})||Fashion[0];
      console.log(Selector,"selector222")
      
      
-
+          
 
     
 
@@ -25,18 +30,18 @@ function Headertooltip({data})
 
 
     return (<div className="total_tip_total_part">
-        <div className="total_tip_total_part_flex">
+        <div className="total_tip_total_part_flex"> 
         <div>
             {/* <p>Men's top Wear</p>
             <p>Men's top Wear</p>
             <p>Men's top Wear</p> */}
-            {Fashion.map((e)=> <Firsttool id={e.id}  data={e}   />)}
+            {Fashion.map((e)=> <Firsttool className="total_first_tool" id={e.id}  data={e}   />)}
         </div>
         <div>
         {/* <p>Men's top Wear</p>
         <p>Men's top Wear</p>
         <p>Men's top Wear</p> */}
-         {<Secontool data={Selector}/>}
+         {<Secontool className="total_second_tool" data={Selector}/>}
         </div>
         </div>
     </div>)
@@ -47,30 +52,30 @@ function Firsttool({data,id})
 {
 
 
-    
+
+    let [arrow,setarrow] = useState(false);
     let dispatch = useDispatch();
-    
-   
-        
-      
     
 
            
     let {categories,subcategories} = data;
  
-   
-   
+
   
  const changehandle=()=>{
 
       
        
     dispatch(subcategoriespass(subcategories))
+    setarrow(true)
      
+ }
+ const arrowhandle=()=>{
+    setarrow(false)
  }
    
      
-    return (<p onMouseEnter={changehandle}>{categories}</p>)
+    return (<p className="linkcategories" onMouseLeave={arrowhandle} onMouseEnter={changehandle}><Link className="link_nodecoration" to="j">{categories}{arrow&&<span><AiOutlineRight/></span>}</Link></p>)
 }
 
 function Secontool({data})
@@ -83,5 +88,5 @@ function Secontool({data})
    
     //  console.log(data,"secondtooltip")
  
-     return (data.map((e)=>{return <p>{e.title}</p>}))
+     return (<> { data.map((e)=>{return <p className="linkcategories"><Link className="link_nodecoration"    to="k">{e.title}</Link></p>})}</>)
 }
